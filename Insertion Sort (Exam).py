77af869b1e6d447f9af9d1111fdc42242c820c73
Abdallah Ibrahem <112083552+abda11ah1brahem@users.noreply.github.com>
2023-03-08 23:03:30 +0200

    Add files via upload

---
+++ Insertion Sort (Exam).py
@@ -0,0 +1,13 @@
+def InsertionSort(A):
+   for i in range (1, len(A)):
+      print("=====\ni=", i, " ", A)
+      temp = A[i]
+      k=i
+      while k>0 and temp < A[k-1]:
+         print("k = ",k,"",A)
+         A[k] = A[k-1]
+         k-=1
+      A[k]=temp
+A= [54,26,93,17,77,3 ,44,55,20]
+InsertionSort(A)
+print(A)
